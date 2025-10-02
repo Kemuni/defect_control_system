b0dfe82614134860e2135ed30e978d3323dbe002
JFurB <dimabondarenko471@gmail.com>
2025-10-02 19:45:47 +0300

    Добавил компонент для отображения состояния на пустых страницах.

---
+++ frontend/components/PagePlaceHolder.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Typography} from "@/components/Typography";
+import {cn} from "@/lib/utils";
+
+export interface PlaceHolderProps extends React.HTMLAttributes<HTMLDivElement> {
+  text: string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+/**
+ * Компонент-заглушка для отображения различных состояний.
+ */
+const PagePlaceHolder: React.FC<PlaceHolderProps> = ({className, text, icon: Icon, ...props}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col flex-1 items-center justify-center text-secondary-hint",
+        className
+      )}
+      {...props}
+    >
+      <Icon className="w-12 h-12"/>
+      <Typography variant="title2" weight="medium" className="text-inherit">{ text }</Typography>
+    </div>
+  );
+};
+export default PagePlaceHolder;
